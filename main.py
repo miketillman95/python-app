50f518707eafbf358902ba9823af49370ce7e3a3
Mike Tillman <59715611+miketillman95@users.noreply.github.com>
2022-03-23 03:15:17 +0000

    adding model

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 # Create a flask app
 app = Flask(
   __name__,
   template_folder='templates',
-  static_folder='static'
+  static_folder='static',
 )
 
 # Index page rendering 'index.html'
 @app.route('/')
 def hello():
   return render_template('index.html')
+
+
+incomes = [
+  { 'description': 'salary', 'amount': 5000 }
+]
+
+
+@app.route('/incomes')
+def get_incomes():
+  return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+  incomes.append(request.get_json())
+  return '', 204
+
 
 if __name__ == '__main__':
   # Run the Flask app
